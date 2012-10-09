ebc27e85cacb91d2491a0ae6753c36130ced9a6b
Scott Albertson <salbertson@ezpublishing.com>
2012-10-08 19:20:29 -0700

    Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planenvy::Application.config.secret_token = '9cb17e73775dd91d178b444632ee8fef3e8266771044a4fd92b793b50b77c7eef29d1ea1abe5578e411d230ab79bdaa0aff115fbd76b546cb9bf7afd38369316'
