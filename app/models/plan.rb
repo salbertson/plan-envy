ca56d772b08b8dc4cf319d8d40e58ec4890f6d07
Scott Albertson <salbertson@ezpublishing.com>
2012-10-10 14:55:01 -0700

    Add price to plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :description, :name, :plan_file, :preview_image
+  attr_accessible :description, :name, :plan_file, :preview_image, :price
 
   has_attached_file :plan_file
   has_attached_file :preview_image, styles: {medium: "500x300>", thumb: "120x120>"}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121010031724) do
+ActiveRecord::Schema.define(:version => 20121010213455) do
 
   create_table "plans", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20121010031724) do
     t.datetime "plan_file_updated_at"
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+    t.decimal  "price"
   end
 
   create_table "users", :force => true do |t|
